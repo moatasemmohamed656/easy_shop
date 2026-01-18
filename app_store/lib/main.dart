5a79ba33c1a58ff8a3eae81aa8a243878ce8f09d
Moatasem Mohamed <moatasemmohamed656@gmail.com>
2026-01-18 19:19:26 +0200

    firebase android setup done

--- app_store/lib/main.dart
+++ app_store/lib/main.dart
@@ -1,7 +1,26 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:app_store/store_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid || Platform.isIOS
+      ? await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyD1jy8Qy5Jmi3XZQUy2CvYDvA5NF7I8qRU",
+          appId: '1:212579227838:android:ade93788bd565b773666ef',
+          messagingSenderId: '212579227838',
+          projectId: 'easyshop-1c2e4', 
+      ))
+      : await Firebase.initializeApp();
+
+  await Firebase.initializeApp();
+
+
   runApp(const StoreApp());
 }
 
