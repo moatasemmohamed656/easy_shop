80637c76495a5604bacaf7f08405d62b2f10c7d7
Moatasem Mohamed <moatasemmohamed656@gmail.com>
2026-01-18 20:53:55 +0200

    env created done

--- app_store/lib/main.dart
+++ app_store/lib/main.dart
@@ -1,26 +1,42 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:app_store/store_app.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:app_store/core/app/env.variables.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
-  Platform.isAndroid || Platform.isIOS
-      ? await Firebase.initializeApp(
+  // Initialize environment variables
+  await EnvVariables.instance.init(
+    envType: EnvTypeEnum.dev,
+  );
+
+  // Initialize Firebase
+  try {
+    if (Platform.isAndroid || Platform.isIOS) {
+      await Firebase.initializeApp(
         options: const FirebaseOptions(
           apiKey: "AIzaSyD1jy8Qy5Jmi3XZQUy2CvYDvA5NF7I8qRU",
           appId: '1:212579227838:android:ade93788bd565b773666ef',
           messagingSenderId: '212579227838',
-          projectId: 'easyshop-1c2e4', 
-      ))
-      : await Firebase.initializeApp();
+          projectId: 'easyshop-1c2e4',
+        ),
+      );
+    } else {
+      await Firebase.initializeApp();
+    }
+  } catch (e) {
+    print('Firebase initialization error: $e');
+  }
 
-  await Firebase.initializeApp();
-
-
-  runApp(const StoreApp());
+  // Force portrait orientation
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    // Run the app
+    runApp(const StoreApp());
+  });
 }
-
